e237d67e8bda211865c4a893e5196687d5a95810
Dowingzlord <sem639107@gmail.com>
2024-11-17 22:54:44 +0100

    Add files via upload

---
+++ cos-101-assignment.py
@@ -0,0 +1,34 @@
+print("formular for area of a square is P =4a")
+# P = 4 * a(number of sides)
+# let a be the number of sides
+# from pickletools import float8
+a =float( input("enter number of sides: "))
+print(a * 4)
+
+
+
+print("formular for area of a triangle = 1/2(b*h")
+# lets use 0.5 instead of 1/2
+# 0.5 * b*h
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+print(0.5 * base * height)
+
+print("formular for diameter of a circle is d=2r")
+# d= 2*r
+r = float(input("enter the radius: "))
+print( 2*r )
+
+
+print("formular for finding force is mass * acceleration ")
+mass = float(input("Enter the mass of the object: "))
+acceleration = float(input("Enter the acceleration: "))
+print( mass * acceleration)
+
+
+print("formular for finding speed =d/t (d=distance, t=time)")
+distance=float(input ("enter the distance: "))
+time=float(input ("enter the  time: " ))
+print( distance / time)
+
+
